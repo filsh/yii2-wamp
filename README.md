0e7edcce41a5dd142141473c10ec67b60b586299
Igor Maliy <imaliy.filsh@gmail.com>
2015-05-16 14:14:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# yii2-runner
+
+## Installation
+
+It is recommended that you install the Gearman library [through composer](http://getcomposer.org/). To do so, add the following lines to your ``composer.json`` file.
+
+```json
+{
+    "require": {
+       "filsh/yii2-wamp-router": "dev-master"
+    }
+}
+```
+
+## Examples
+
+```php
+// configure component
+'components' => [
+  'wampRouter' => [
+      'class' => 'filsh\wamprouter\WampRouter',
+      'realm' => 'realm',
+      'host' => '172.17.0.20',
+      'port' => '8000',
+  ]
+],
+
+// run examples
+Yii::$app->wampRouter->procedure(function(Connection $connection, ClientSession $session) {
+    $session->call('com.myapp.add2', [2, 3])
+        ->then(function (CallResult $result) use($connection) {
+                echo $result;
+                $connection->close();
+            },
+            function (ErrorMessage $error) use($connection) {
+                echo $error;
+                $connection->close();
+            }
+        );
+});
+
+```
---
+++ WampRouter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace filsh\wamprouter;
+
+use Thruway\Connection;
+use Thruway\ClientSession;
+use Thruway\Logging\Logger;
+use Psr\Log\NullLogger;
+
+class WampRouter extends \yii\base\Component
+{
+    public $host;
+    
+    public $port;
+    
+    public $realm;
+    
+    public $timeuot = 5;
+    
+    public $enableLogging = true;
+    
+    public function init()
+    {
+        if(empty($this->host) || empty($this->port) || empty($this->realm)) {
+            throw new \yii\base\InvalidConfigException('Invalid Wamp Router config.');
+        }
+        parent::init();
+    }
+    
+    public function procedure(\Closure $callback, array $connectionOptions = [])
+    {
+        if(!$this->enableLogging) {
+            Logger::set(new NullLogger());
+        }
+        
+        ob_start();
+        
+        $connection = $this->createConnection($connectionOptions);
+        $connection->once('open', function (ClientSession $session) use($callback, $connection) {
+            call_user_func_array($callback, [$connection, $session]);
+        });
+
+        $connection->open();
+        
+        if($this->enableLogging) {
+            \Yii::info(ob_get_clean());
+        } else {
+            ob_clean();
+        }
+    }
+    
+    protected function createConnection(array $options = [])
+    {
+        $connection = new Connection(array_merge($options, [
+            'realm'   => $this->realm,
+            'url'     => strtr('ws://{host}:{port}', ['{host}' => $this->host, '{port}' => $this->port])
+        ]));
+        
+        $loop = $connection->getClient()->getLoop();
+        $timer = $loop->addTimer($this->timeuot, function () use ($loop) {
+            $loop->stop();
+        });
+        $connection->once('close', function() use($timer) {
+            $timer->cancel();
+            \Yii::warning('WAMP connection closed by timout.');
+        });
+        
+        return $connection;
+    }
+}
