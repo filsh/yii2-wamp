196e51771fd48604da642ba11ca8f3ec569c9582
Igor Maliy <imaliy.filsh@gmail.com>
2015-05-19 08:44:30 +0300

    Wamp exception

---
+++ WampException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace filsh\wamprouter;
+
+use Thruway\Message\ErrorMessage;
+
+class WampException extends \yii\base\Exception
+{
+    public $errorUri;
+    
+    public $errorMessage;
+    
+    public function __construct(ErrorMessage $error, \Exception $previous = null)
+    {
+        $this->errorUri = $error->getErrorURI();
+        $this->errorMessage = ucfirst(implode(': ', $error->getArguments()));
+        
+        parent::__construct($this->errorMessage, $error->getMsgCode(), $previous);
+    }
+    
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        $this->errorUri;
+    }
+}
