3222fe961a447d43a71c1c39cf040b35d1671c6e
Igor Maliy <imaliy.filsh@gmail.com>
2015-09-18 01:53:04 +0300

    Optimization

--- locator/runners/file/Upload.php
+++ locator/runners/file/Upload.php
@@ -8,17 +8,11 @@ class Upload extends \filsh\wamp\locator\runners\File
     
     public $type;
     
-    public $domain;
-    
-    public $https = true;
-    
     protected function getKwParams()
     {
         return [
             'url' => $this->url,
             'type' => $this->type,
-            'domain' => $this->domain,
-            'https' => $this->https
         ];
     }
 }
